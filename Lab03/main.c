6ac8e6dd601eb8b3030b85d3f6bed38a226a6099
Einthan <98476895+Einthan@users.noreply.github.com>
2022-01-27 15:12:35 -0600

    Add files via upload

---
+++ Lab03/main.c
@@ -0,0 +1,15 @@
+/* main.c simple program to test assembler program */
+
+#include <stdio.h>
+
+extern long long int test(long long int a, long long int b);
+extern long long int lab03b();
+extern long long int lab03c();
+int main(void)
+{
+	long long int b = lab03b();
+	printf("Result of final uin = %ld\n", b);
+    //long long int a = test(3,5);
+    //printf("Result of test(3, 5) = %ld\n", a);
+    return 0;
+}
